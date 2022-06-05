9734c434786dd7f55606f2b630fb12ea95957cd3
bishalsth <bishal.sth.3@gmail.com>
2022-06-05 10:39:34 +0545

    Title added

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,6 +3,7 @@ import React from 'react'
 function TodoList({todos}) {
   return (
     <div>
+            <h2 className='text-center'>TODOS LIST</h2>
         <ul className='list-group'>
             {todos.map((todo)=>  <li class="list-group-item d-flex justify-content-between align-items-center">
             {todo.title}
